398644e0e660963872303ddb59220d8aef7df354
Davi Gabriel <47298896+daviziih@users.noreply.github.com>
2023-09-25 20:48:16 -0300

    Update aula2509.sql

--- aula2509.sql
+++ aula2509.sql
@@ -16,3 +16,26 @@ begin
     end if;
 	dbms_output.put_line('Fim do Programa');
 end;
+
+
+/* Atividade 2 */
+
+declare 
+	contador number := 50;
+
+begin
+	<<inicio>>
+    if contador <= 99 then
+		if contador MOD 4 = 0 then
+    		dbms_output.put_line(contador);
+            contador := contador + 1;
+			goto inicio;
+		end if;
+		if contador MOD 2 = 0 then
+    		contador := contador + 1;
+			goto inicio;
+		end if;
+		contador := contador + 1;
+		goto inicio;
+	end if;
+end;
